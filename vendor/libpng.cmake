3b72dd4c1315a84accd8d907d39a4742ba63647f
Vladimir Voitenko <vladimirdev635@gmail.com>
2024-09-14 17:00:23 +0500

    fix

--- vendor/libpng.cmake
+++ vendor/libpng.cmake
@@ -8,9 +8,9 @@ set(PNG_FRAMEWORK OFF CACHE BOOL "")
 set(PNG_SHARED OFF CACHE BOOL "")
 set(PNG_TESTS OFF CACHE BOOL "")
 set(PNG_TOOLS OFF CACHE BOOL "")
+include_directories(
+    BEFORE ${ZLIB_INCLUDE_DIRS}
+)
 set(PNG_ZLIB_VERNUM 0 CACHE BOOL "")
 FetchContent_MakeAvailable(libpng)
-target_include_directories(
-    png_static PRIVATE "$<TARGET_PROPERTY:ZLIB::ZLIB,INCLUDE_DIRECTORIES>"
-)
 get_target_property(png_static_INCLUDE_DIRS png_static INCLUDE_DIRECTORIES)
