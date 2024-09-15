fd9bb1f48db8b75f296ced67c3dc7aedeff4d2b4
Vladimir Voitenko <vladimirdev635@gmail.com>
2024-09-15 11:52:57 +0500

    vendor:zlib:fix

--- vendor/zlib.cmake
+++ vendor/zlib.cmake
@@ -2,16 +2,17 @@ include(FetchContent)
 FetchContent_Declare(
     ZLIB
     GIT_REPOSITORY https://github.com/madler/zlib
-    GIT_TAG v1.2.10
+    GIT_TAG v1.2.12
     OVERRIDE_FIND_PACKAGE
 )
 set(ZLIB_BUILD_EXAMPLES OFF CACHE INTERNAL "")
 set(SKIP_INSTALL_ALL ON CACHE INTERNAL "")
 FetchContent_MakeAvailable(ZLIB)
 set_target_properties(zlib PROPERTIES EXCLUDE_FROM_ALL 1 EXCLUDE_FROM_DEFAULT_BUILD 1)
+target_compile_options(zlibstatic PRIVATE -Wno-deprecated-non-prototype)
 if (${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
     set_target_properties(zlibstatic PROPERTIES OUTPUT_NAME zd)
 endif()
 FetchContent_GetProperties(ZLIB)
 add_library(ZLIB::ZLIB ALIAS zlibstatic)
-get_target_property(ZLIB_INCLUDE_DIRS ZLIB::ZLIB INCLUDE_DIRECTORIES)
+set(ZLIB_INCLUDE_DIRS "${zlib_SOURCE_DIR};${zlib_BINARY_DIR}")
